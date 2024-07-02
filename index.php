a65d30d7c38a644defc14378211d1683006bb226
jetx1712 <87372329+jetx1712@users.noreply.github.com>
2024-07-02 17:42:28 -0300

    comit 1

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Campanha provisoria</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a href="#">
+            <img class="navbar-brand-img" src="assets/imagens/logo.png" />
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#ancor-acampanha">A CAMPANHA</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#ancor-como-identificar">COMO IDENTIFICAR?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#ancor-denuncie">DENUNCIE</a>
+                </li>
+            </ul>
+        </div>
+        <div id="botao-contato">
+            <div class="botao-direito-nav-1">
+                <img src="assets/imagens/Icon_balao_ouvidoria.svg" />
+            </div>
+            <div class="botao-direito-nav-2">
+                <label>Canal de Ouvidoria<br><span>0800 374 0100</span></label>
+
+            </div>
+        </div>
+    </nav>
+    <section id="banner-home">
+        <div>
+            <h1>Não tenha <br> Medo!</h1>
+        </div>
+    </section>
+    <section id="banner-2-campanha"><a style="position: absolute; top: 1600px;" id="ancor-acampanha">a</a></section>
+    <section id="acampanha">
+        <div class="container">
+            <div class="titulo">
+                <h1>A CAMPANHA</h1>
+                <h3>Denuncie! Assédio é crime!</h3>
+            </div>
+            <div class="conteudo-campanha">
+                <p>
+                    Os assédios moral e sexual no ambiente de trabalho representam uma violação à dignidade e é nosso
+                    dever combatê-los. Ambos são previstos como crimes e passíveis de penalidades, como prisão.
+                </p><br>
+                <P>
+                    Se um colaborador se sente sistematicamente exposto, humilhado e constrangido por determinada
+                    situação durante o exercício do seu trabalho, ele é vítima de assédio moral. O constrangimento se dá
+                    por meio
+                    de condutas abusivas, comportamentos, palavras, atos, gestos e escritos que possam trazer danos à
+                    personalidade, à dignidade ou à integridade física ou psíquica de uma pessoa.
+                </p><br>
+                <p>
+                    O assédio sexual
+                    decorre de situações de constrangimento de colegas por meio de insinuações constantes ou até mesmo
+                    toque físico que tenham como objetivo obter vantagem ou favorecimento sexual. Geralmente as mulheres
+                    são as mais afetadas nesses casos. O assediador pode fazer piadas pejorativas à sexualidade ou à
+                    orientação sexual da pessoa, brincadeiras sexistas ou comentários constrangedores sobre o gênero
+                    oposto.
+                </p><br>
+                <a id="ancor-como-identificar"></a>
+                <p>
+                    Queremos conscientizar os colaboradores da Statled Brasil sobre os assédios moral e sexual e
+                    deixar claro que a empresa não tolera nenhum dos casos.
+                </P>
+            </div>
+            <div class="titulo">
+                <h3 style="font-size: 30px">QUEBRE O SILÊNCIO! DENUNCIE!</h3>
+            </div>
+        </div>
+    </section>
+    <section id="como-identificar">
+        <div class="container">
+            <div class="titulo">
+                <h1>COMO IDENTIFICAR?</h1>
+                <h3>10 situações que configuram assédio moral e sexual</h3>
+            </div>
+            <div class="col-md-12 quadrados">
+                <div style="background: #ea811f" class="col-md-3 quadrados-internos">
+                    <h2>Configura assédio moral quando o superior ou colega …</h2>
+                </div>
+                <div class="col-md-3 quadrados-internos">
+                    <h1>1</h1>
+                    <h3>Tira a autonomia do colaborador ou contesta constantemente suas decisões.</h3>
+                </div>
+                <div class="col-md-3 quadrados-internos">
+                    <h1>2</h1>
+                    <h3>Sobrecarrega o colaborador ou tira o trabalho que ele realizava para provocar a sensação de
+                        inutilidade ou incompetência.</h3>
+                </div>
+            </div>
+            <div class="col-md-12 quadrados">
+                <div class="col-md-3 quadrados-internos">
+                    <h1>3</h1>
+                    <h3>Passa tarefas humilhantes ou impossíveis de serem cumpridas.</h3>
+
+                </div>
+                <div class="col-md-3 quadrados-internos">
+                    <h1>4</h1>
+                    <h3>Grita ou fala de forma desrespeitosa.</h3>
+
+                </div>
+                <div class="col-md-3 quadrados-internos">
+                    <h1>5</h1>
+                    <h3>Desconsidera a opinião e a sugestão do colaborador.</h3>
+                </div>
+            </div>
+            <hr />
+            <div class="col-md-12 quadrados">
+                <div style="background: #ea811f" class="col-md-3 quadrados-internos">
+                    <h2>Configura assédio sexual quando o colega de trabalho ou chefe…</h2>
+                </div>
+                <div class="col-md-3 quadrados-internos">
+                    <h1>1</h1>
+                    <h3>Insiste em conversas indesejáveis sobre sexo.</h3>
+                </div>
+                <div class="col-md-3 quadrados-internos">
+                    <h1>2</h1>
+                    <h3>Faz contato físico indesejado</h3>
+                </div>
+            </div>
+            <div class="col-md-12 quadrados">
+                <div class="col-md-3 quadrados-internos">
+                    <h1>3</h1>
+                    <h3>Pressiona por encontros e faz convites impertinentes.</h3>
+
+                </div>
+                <div class="col-md-3 quadrados-internos">
+                    <h1>4</h1>
+                    <h3>Faz insinuações de caráter sexual, explícitas ou veladas.</h3>
+
+                </div>
+                <div class="col-md-3 quadrados-internos">
+                    <h1>5</h1>
+                    <h3>Faz ameaças e chantagens em troca de manter o emprego ou de promover o(a) colaborador(a)</h3>
+                </div>
+            </div>
+            <a id="ancor-denuncie"></a>
+            <div class="botao-download">
+                <a href="#"><button><i class="fas fa-arrow-right"></i> FAÇA O DOWNLOAD DA CARTILHA DA NOSSA
+                        CAMPANHA</button></a>
+            </div>
+        </div>
+    </section>
+    <section id="contato">
+        <div class="container">
+            <div class="titulo">
+                <h1>DENUNCIE AQUI! ASSÉDIO É CRIME!</h1>
+            </div>
+            <h2>Você quer se identificar?</h2>
+            <div class="identificacao">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="identify" id="radioYes" value="yes"
+                        onclick="toggleForm()">
+                    <label class="form-check-label" for="radioYes">
+                        Sim
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="identify" id="radioNo" value="no"
+                        onclick="toggleForm()">
+                    <label class="form-check-label" for="radioNo">
+                        Não
+                    </label>
+                </div>
+            </div>
+            <div class="formulario">
+                <form id="identificationForm" class="hidden-form col-md-6">
+                    <h2>Preencha com seus dados abaixo</h2>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" placeholder="Digite seu nome">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="email" placeholder="Digite seu email">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" name="mensagem"
+                            placeholder="Digite a sua mensagem aqui"></textarea>
+                    </div>
+                    <div style="display: flex;align-items: center;justify-content: center;">
+                        <button type="submit"><i class="fas fa-arrow-right"></i> Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <section id="footer">
+        <div class="container">
+            <div class="info-footer">
+                <img src="assets/imagens/logo.png">
+                <h5>Cumprindo a exigência da Lei nº 14.457, de 21 de setembro de 2022 (Processo nº 19966.100910/2021-44)
+                </h5>
+                <div class="social">
+                    <i class="fa fa-facebook-f"></i>
+                    <i class="fa fa-linkedin"></i>
+                    <i class="fa fa-instagram"></i>
+
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <!-- Bootstrap JS e dependências -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        function toggleForm() {
+            const form = document.getElementById('identificationForm');
+            const radioYes = document.getElementById('radioYes');
+            form.style.display = radioYes.checked ? 'block' : 'none';
+        }
+        document.querySelectorAll('a.nav-link').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+    </script>
+</body>
+
+</html>
